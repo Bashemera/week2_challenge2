79ecb14a60d7bd01852d5ce0d6ed22346ffadaf6
unknown <bbashemera@gmail.com>
2019-02-14 12:41:40 -0800

    feat:edit 2

--- dictionaries.py
+++ dictionaries.py
@@ -1,6 +1,10 @@
-dictionary=dict()
-for dictionaryvalues in range(1,16):
-    dictionary[dictionaryvalues]=dictionaryvalues**2
-print(dictionary) 
-            
+def displayKeyValuePairs():
+    dictionary=dict()
+    for dictionaryvalues in range(1,16):
+        dictionary[dictionaryvalues]=dictionaryvalues**2
+    print(dictionary)
+
+if __name__=="__main__":
+    displayKeyValuePairs()
+                
     
