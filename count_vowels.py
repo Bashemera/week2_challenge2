96f3fa8ddb1aba72873dd8da54a15865dc717a34
unknown <bbashemera@gmail.com>
2019-02-25 22:26:44 +0300

    feat: new Commit

--- count_vowels.py
+++ count_vowels.py
@@ -3,6 +3,8 @@ def count_vowels(input_string):
     vowels_list = list(input_string)
     vowels_in_string=[]
     vowels_list_without_duplicates=[]
+    result=''
+    results=[]
     final_tuple=()
     length_turple =()
     result= ''
@@ -13,14 +15,19 @@ def count_vowels(input_string):
     for vowel in vowels_in_string: 
         if vowel not in vowels_list_without_duplicates: 
             vowels_list_without_duplicates.append(vowel)
+    for element in vowels_list_without_duplicates:
+        result += str(element)
+    
+    results.append(result)
             
-    no_duplicates_tuple=tuple(vowels_list_without_duplicates)
+    no_duplicates_tuple=tuple(results)
+    
     tuple_length =len(no_duplicates_tuple)
     length_turple += (tuple_length,)
     final_tuple=(no_duplicates_tuple) +length_turple
     print(final_tuple)
     
-count_vowels("Museveni")
+count_vowels("Bashemera")
 
                 
     
